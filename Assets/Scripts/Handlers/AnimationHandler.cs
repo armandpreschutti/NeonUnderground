3c5c12a3e55db2b72402d8730352a7092794ab42
armandpreschutti <armand.preschutti@gmail.com>
2023-07-01 16:16:32 -0400

    Impemented Combo System
    Designed and implemented a prototype combo system. This system allows for 3 light attacks to be executed in rapid succession. This is a prototype and will be updated in future commits

--- Assets/Scripts/Handlers/AnimationHandler.cs
+++ Assets/Scripts/Handlers/AnimationHandler.cs
@@ -1,10 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
     public Animator anim;
+    public bool isBlocking;
+    public bool isAttacking;
+    public bool canCombo;
+    public bool canFinish;
+    public bool LightAttack1;
+    public bool LightAttack2;
+
+
 
     public void Start()
     {
@@ -12,18 +22,79 @@ public class AnimationHandler : MonoBehaviour
     }
 
     public void Update()
+    { 
+        Blocking();
+        LightAttack();
+    }
+    public void Blocking()
     {
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButton("Fire2") && !isAttacking)
         {
             anim.SetBool("IsBlocking", true);
+            isBlocking = true;
         }
         else
         {
+            isBlocking = false;
             anim.SetBool("IsBlocking", false);
         }
-        if (Input.GetButtonDown("Fire1"))
+    }
+    public void LightAttack()
+    {
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
         {
-            anim.SetTrigger("IsLightAttacking");
+         
+            anim.SetBool("LightAttack1", true);            
+        }
+        
+        else if(Input.GetButtonDown("Fire1") && canCombo && isAttacking)
+        {
+            canCombo= false;
+            anim.SetBool("LightAttack2", true);
+        }
+        else if (Input.GetButtonDown("Fire1") && canFinish && isAttacking)
+        {
+            canFinish= false;
+            anim.SetBool("LightAttack3", true);
+        }
+        else
+        {
+            return;
         }
     }
+    public void EndAttack()
+    {
+        isAttacking = false;
+        canCombo= false;
+        canFinish= false;
+        anim.SetBool("IsAttacking", false);
+    }
+    public void StartAttack()
+    {
+        isAttacking = true;
+        anim.SetBool("IsAttacking", true);
+    }
+    public void DetectCombo()
+    {
+        canCombo= true;
+    }
+    public void DetectFinisher()
+    {
+        canFinish= true;
+    }
+    public void EndLightAttack1()
+    {
+        anim.SetBool("LightAttack1", false);
+    }
+    public void EndLightAttack2()
+    {
+        anim.SetBool("LightAttack2", false);
+        EndLightAttack1();
+    }
+    public void EndLightAttack3()
+    {
+        anim.SetBool("LightAttack3", false);
+       EndLightAttack2();
+    }
+
 }
