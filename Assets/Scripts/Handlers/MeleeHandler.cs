a0e4094e10c08b88b6638419c3252620b1639a85
armandpreschutti <armand.preschutti@gmail.com>
2023-07-06 00:25:27 -0400

    Added new features. Further description to follow.

--- Assets/Scripts/Handlers/MeleeHandler.cs
+++ Assets/Scripts/Handlers/MeleeHandler.cs
@@ -21,6 +21,8 @@ public class MeleeHandler : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
 
+    public Vector3 combatDistance;
+
     public GameObject weapon;
     public RuntimeAnimatorController[] styles;
     public GameObject dummy;
@@ -66,9 +68,13 @@ public class MeleeHandler : MonoBehaviour
 
     public void Update()
     {
-        DetectAttack();
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        DetectAttack();
+        anim.SetFloat("Horizontal", horizontalInput);
+        anim.SetFloat("Vertical", verticalInput);
+        
+        
 
 
     }
@@ -76,8 +82,24 @@ public class MeleeHandler : MonoBehaviour
     {
         if (isLockedOn && target != null)
         {
-            RotateTowardsTarget();
-            Strafe();
+            FaceTarget();
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("Block") && !anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            {
+                
+                Strafe();
+            }
+            else if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            {
+                AttackMovement();
+            }
+            else
+            {
+                return;
+            }
+            Vector3 distance = target.GetComponent<Rigidbody>().position - rb.position;
+            /*Debug.Log(distance);
+            Debug.Log(distance.sqrMagnitude);*/
+
         }
     }
 
@@ -86,7 +108,7 @@ public class MeleeHandler : MonoBehaviour
         if (!isAttacking && !isBlocking) 
         {             
             anim.SetTrigger("Attack");
-            AttackMovement();
+           
         }
     }
     public void StartBlock(InputAction.CallbackContext context)
@@ -135,7 +157,7 @@ public class MeleeHandler : MonoBehaviour
         }
     }
     
-    public void AttackMovement()
+    public void ChargeMovement()
     {
         Vector3 direction = (dummy.gameObject.transform.position - transform.position).normalized;
         rb.AddForce(direction * movementSpeed, ForceMode.VelocityChange);
@@ -158,7 +180,7 @@ public class MeleeHandler : MonoBehaviour
         anim.runtimeAnimatorController = styles[2];
         isEquiped = true;
     }
-    private void RotateTowardsTarget()
+    private void FaceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion targetRotation = Quaternion.LookRotation(direction);
@@ -167,11 +189,32 @@ public class MeleeHandler : MonoBehaviour
 
     private void Strafe()
     {
-
-
+        Vector3 distance = target.GetComponent<Rigidbody>().position - rb.position;
         Vector3 strafeDirection = transform.right * horizontalInput + transform.forward * verticalInput;
         rb.MovePosition(rb.position + strafeDirection * strafeSpeed * Time.deltaTime);
+        /*if (distance.sqrMagnitude > combatDistance.sqrMagnitude)
+        {
+            Vector3 strafeDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+            rb.MovePosition(rb.position + strafeDirection * strafeSpeed * Time.deltaTime);
+        }*/
+            
+        
     }
+    public void AttackMovement()
+    {
+        Vector3 distance = target.GetComponent<Rigidbody>().position - rb.position;
+        Vector3 newPosition = Vector3.Lerp(rb.position, target.GetComponent<Rigidbody>().position, /*anim.GetCurrentAnimatorStateInfo(0).normalizedTime*/ movementSpeed * Time.deltaTime);
+        rb.MovePosition(newPosition);
+        /*if (distance.sqrMagnitude > combatDistance.sqrMagnitude)
+        {
+            Vector3 newPosition = Vector3.Lerp(rb.position, target.GetComponent<Rigidbody>().position, anim.GetCurrentAnimatorStateInfo(0).normalizedTime movementSpeed * Time.deltaTime);
+            rb.MovePosition(newPosition);
+        }
+        else
+        {
+            return;
+        }*/
 
+    }
 
 }
