76f0b0ee116ac86a909c20d40e354e7ed2a44463
armandpreschutti <armand.preschutti@gmail.com>
2023-07-28 15:44:49 -0400

    Stripped project of all unnecessary files
    Deleted all un needed prototype files from project and imported new TKD animations to use for the next phase of prototyping. More details to come in future commits.

--- Assets/Scripts/Scriptable Objects/AttackSO.cs
+++ Assets/Scripts/Scriptable Objects/AttackSO.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Attacks/Light Attack")]
+[CreateAssetMenu(menuName = "Actions/Attack")]
 
 public class AttackSO : ScriptableObject
 {
     public AnimatorOverrideController animatorOV;
-    public float damage; 
 }
